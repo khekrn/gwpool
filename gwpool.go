cb340f8d02f142507b6b6f033440a6d4e7e0810f
khekrn <kishorenayark@gmail.com>
2024-08-19 15:20:24 +0530

    Adding necessary comments in gwpool.go which is essential for better code readability

--- gwpool.go
+++ gwpool.go
@@ -23,23 +23,34 @@ type task func() error
 
 // workerPool represents a pool of worker
 type workerPool struct {
-	workers        []*worker
-	workerStack    []int
-	taskQueue      chan task
-	ctx            context.Context
-	cancelFunc     context.CancelFunc
-	timeout        time.Duration
+	// Each worker with a channel of size 1.
+	workers []*worker
+	// Available workers are defined via slice.
+	workerStack []int
+
+	// Tasks are added to this channel first and later dispatched to workers.
+	taskQueue chan task
+	ctx       context.Context
+	// Used to cancel the context and it's ued in Release function.
+	cancelFunc context.CancelFunc
+	// Set by WithTimeout(), used to set a timeout for a task. Default is 0, which means no timeout.
+	timeout time.Duration
+	// adjustInterval is the interval to adjust the number of workers. Default is 1 second.
 	adjustInterval time.Duration
 
 	lock sync.Locker
 	cond *sync.Cond
 
-	maxWorkers    int
-	minWorkers    int
+	maxWorkers int
+	// Set by WithMinWorkers(), used to adjust the number of workers. Default equals to maxWorkers.
+	minWorkers int
+	// Set by WithTaskQueue(), used to set the size of the task queue. Default is 1e6.
 	taskQueueSize int
-	retryCount    int
+	// Set by WithRetryCount(), used to retry a task when it fails. Default is 0.
+	retryCount int
 }
 
+// NewWorkerPool creates a new pool of workers
 func NewWorkerPool(maxWorkers int, opts ...Option) WorkerPool {
 	ctx, cancel := context.WithCancel(context.Background())
 	pool := &workerPool{
@@ -79,10 +90,12 @@ func NewWorkerPool(maxWorkers int, opts ...Option) WorkerPool {
 	return pool
 }
 
+// AddTask adds a task to the pool.
 func (w *workerPool) AddTask(t task) {
 	w.taskQueue <- t
 }
 
+// TryAddTask adds a task to the pool only if enough workers are available.
 func (w *workerPool) TryAddTask(t task) bool {
 	w.lock.Lock()
 	currentLength := len(w.workerStack)
@@ -95,6 +108,7 @@ func (w *workerPool) TryAddTask(t task) bool {
 	return result
 }
 
+// Wait waits for all tasks to be dispatched and completed.
 func (w *workerPool) Wait() {
 	for {
 		w.lock.Lock()
@@ -107,59 +121,47 @@ func (w *workerPool) Wait() {
 	}
 }
 
+// Release stops all workers and releases resources.
 func (w *workerPool) Release() {
 	close(w.taskQueue)
 	w.cancelFunc()
 
+	// Wait for all tasks to be processed
+	w.Wait()
+
 	w.cond.L.Lock()
-	for len(w.workerStack) != w.minWorkers {
+	// Close all worker queues
+	for _, worker := range w.workers {
+		close(worker.workerQueue)
+	}
+	w.cond.L.Unlock()
+
+	// Wait for all workers to finish
+	w.cond.L.Lock()
+	for len(w.workerStack) != len(w.workers) {
 		w.cond.Wait()
 	}
 	w.cond.L.Unlock()
 
-	for _, worker := range w.workers {
-		close(worker.workerQueue)
-	}
-
 	w.workers = nil
 	w.workerStack = nil
-
-	// close(w.taskQueue)
-	// w.cancelFunc()
-
-	// // Wait for all tasks to be processed
-	// w.Wait()
-
-	// w.cond.L.Lock()
-	// // Close all worker queues
-	// for _, worker := range w.workers {
-	// 	close(worker.workerQueue)
-	// }
-	// w.cond.L.Unlock()
-
-	// // Wait for all workers to finish
-	// w.cond.L.Lock()
-	// for len(w.workerStack) != len(w.workers) {
-	// 	w.cond.Wait()
-	// }
-	// w.cond.L.Unlock()
-
-	// w.workers = nil
-	// w.workerStack = nil
 }
 
+// Running returns the number of workers that are currently working.
 func (w *workerPool) Running() int {
 	w.lock.Lock()
 	defer w.lock.Unlock()
 	return len(w.workers) - len(w.workerStack)
 }
 
+// WorkerCount returns the number of workers in the pool.
 func (w *workerPool) WorkerCount() int {
 	w.lock.Lock()
 	defer w.lock.Unlock()
 	return len(w.workers)
 }
 
+// QueueSize returns the size of the task queue.
 func (w *workerPool) QueueSize() int {
 	w.lock.Lock()
 	defer w.lock.Unlock()
@@ -181,6 +183,7 @@ func (w *workerPool) popWorker() int {
 	return workerIndex
 }
 
+// adjustWorkers adjusts the number of workers according to the number of tasks in the queue.
 func (w *workerPool) adjustWorkers() {
 	ticker := time.NewTicker(w.adjustInterval)
 	defer ticker.Stop()
@@ -193,6 +196,7 @@ func (w *workerPool) adjustWorkers() {
 			w.cond.L.Lock()
 			if len(w.taskQueue) > len(w.workers)*3/4 && len(w.workers) < w.maxWorkers {
 				adjustFlag = true
+				// Double the number of workers until it reaches the maximum
 				newWorkers := min(len(w.workers)*2, w.maxWorkers) - len(w.workers)
 				for i := 0; i < newWorkers; i++ {
 					worker := newWorker()
@@ -202,7 +206,9 @@ func (w *workerPool) adjustWorkers() {
 				}
 			} else if len(w.taskQueue) == 0 && len(w.workerStack) == len(w.workers) && len(w.workers) > w.minWorkers {
 				adjustFlag = true
+				// Halve the number of workers until it reaches the minimum
 				removeWorkers := (len(w.workers) - w.minWorkers + 1) / 2
+				// Sort the workerStack before removing workers.
 				sort.Ints(w.workerStack)
 				w.workers = w.workers[:len(w.workers)-removeWorkers]
 				w.workerStack = w.workerStack[:len(w.workerStack)-removeWorkers]
@@ -217,6 +223,7 @@ func (w *workerPool) adjustWorkers() {
 	}
 }
 
+// dispatch dispatches tasks to workers.
 func (w *workerPool) dispatch() {
 	for t := range w.taskQueue {
 		w.cond.L.Lock()
