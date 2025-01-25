7aa18a679f7e4949d333909448ba203fac83ce27
khekrn <kishorenayark@gmail.com>
2025-01-25 12:23:22 +0530

    Adding benchmarks comparing different goroutine pool

--- go.mod
+++ go.mod
@@ -2,3 +2,7 @@ module github.com/khekrn/gwpool
 
 go 1.23
 
+require (
+	github.com/alitto/pond v1.8.3
+	github.com/devchat-ai/gopool v0.6.2
+)
--- gwpool_test.go
+++ gwpool_test.go
@@ -4,6 +4,9 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/alitto/pond"
+	"github.com/devchat-ai/gopool"
 )
 
 func TestAddTask(t *testing.T) {
@@ -17,4 +20,82 @@ func TestAddTask(t *testing.T) {
 		})
 		pool.Wait()
 	})
+}
+
+const (
+	PoolSize = 1e4
+	TaskNum  = 1e6
+)
+
+func BenchmarkGwPool(b *testing.B) {
+	pool := NewWorkerPool(PoolSize)
+	defer pool.Release()
+
+	taskFunc := func() error {
+		time.Sleep(10 * time.Millisecond)
+		return nil
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for num := 0; num < TaskNum; num++ {
+			pool.AddTask(taskFunc)
+		}
+		pool.Wait()
+	}
+	b.StopTimer()
+}
+
+func BenchmarkGoroutines(b *testing.B) {
+	var wg sync.WaitGroup
+	var taskNum = int(TaskNum)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(taskNum)
+		for num := 0; num < taskNum; num++ {
+			go func() (interface{}, error) {
+				time.Sleep(10 * time.Millisecond)
+				wg.Done()
+				return nil, nil
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkPond(b *testing.B) {
+	pool := pond.New(PoolSize, 0, pond.MinWorkers(PoolSize))
+
+	taskFunc := func() {
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < TaskNum; i++ {
+			pool.Submit(taskFunc)
+		}
+		pool.StopAndWait()
+	}
+	b.StopTimer()
+}
+
+func BenchmarkGoPool(b *testing.B) {
+	pool := gopool.NewGoPool(PoolSize)
+	defer pool.Release()
+
+	taskFunc := func() (interface{}, error) {
+		time.Sleep(10 * time.Millisecond)
+		return nil, nil
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for num := 0; num < TaskNum; num++ {
+			pool.AddTask(taskFunc)
+		}
+		pool.Wait()
+	}
+	b.StopTimer()
 }
